371e6d90248f9e6c52930814bea165582dc5e6f3
Oliver Appel <olive@NB-OAP.cbwg.local>
2020-02-09 13:57:34 +0100

    Add project files.

---
+++ Olo42.FileDataAccess.Contracts/IFileDataAccess.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Olo42.FileDataAccess.Contracts
+{
+    public interface IFileDataAccess<T>
+    {
+        T Read(string path);
+
+        void Write(string path, T obj);
+    }
+}
