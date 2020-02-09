b5f34a2ec587ef67358626d9d1ea5401d02c98ed
Olo42 <olo@apck.de>
2020-02-09 22:54:51 +0100

    Use array instead of IEnumerable

--- Olo42.FileDataAccess.Contracts/IFileDataAccess.cs
+++ Olo42.FileDataAccess.Contracts/IFileDataAccess.cs
@@ -1,10 +1,15 @@
 ﻿// Copyright (c) Oliver Appel. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
+using System.IO;
+
 namespace Olo42.FileDataAccess.Contracts
 {
     public interface IFileDataAccess<T>
     {
+        FileInfo[] GetFiles(DirectoryInfo directoryInfo);
+
         T Read(string path);
 
         void Write(string path, T obj);
