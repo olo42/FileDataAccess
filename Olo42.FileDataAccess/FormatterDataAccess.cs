3a9d9fa93fc42ede2f9eaedfadbd372bd4aa43ac
Olo42 <olo@apck.de>
2020-02-10 19:21:55 +0100

    Clean-up usings

--- Olo42.FileDataAccess/FormatterDataAccess.cs
+++ Olo42.FileDataAccess/FormatterDataAccess.cs
@@ -1,10 +1,8 @@
 ﻿// Copyright (c) Oliver Appel. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.IO;
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 using Olo42.FileDataAccess.Contracts;
 
 namespace Olo42.FileDataAccess
